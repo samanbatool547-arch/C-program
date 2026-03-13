20897fa47b19f2fd0feb9a65909777249b21cf5a
samanbatool547-arch <samanbatool547@gmail.com>
2026-03-13 10:16:17 -0800

    C++practice program

---
+++ program 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int num1,num2,Result;
+    cout<<"plz enter two value"<<endl;
+    cin>>num1>>num2;
+    Result=num1=num2
+    cout<<"sum"<<Result<<endl;
+    return0;
+}
