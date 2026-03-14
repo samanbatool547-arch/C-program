a6abcb35a3f1d8a485fb7ba0456b426be25366fb
samanbatool547-arch <samanbatool547@gmail.com>
2026-03-14 07:53:38 -0800

    assignment no 3

---
+++ odd numb.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=1 ,count=0;
+	cout<<"the first 50 odd no"<<endl;
+	do{
+		cout<<i<<endl;
+     	i=i+2;
+     }
+     while(i<50);
+     
+
+
+
+return 0;
+	
+
+}
